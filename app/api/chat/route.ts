2a83a984d44a355475a244e154d7e28274890cf8
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2023-08-11 00:01:47 +0600

    setup

---
+++ app/api/chat/route.ts
@@ -0,0 +1,40 @@
+// ./app/api/chat/route.ts
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+const systemPrompt = {
+  role: "system",
+  content: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.`,
+};
+
+// Create an OpenAI API client (that's edge friendly!)
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  // Extract the `prompt` from the body of the request
+  const { messages } = await req.json();
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages: [
+      systemPrompt,
+      messages.map((message: any) => ({
+        content: message.content,
+        role: message.role,
+      })),
+    ],
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
